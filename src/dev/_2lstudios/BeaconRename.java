6266babe0c7baa11f3919b87c3acf20aebb61ab9
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-08 18:02:22 -0300

    create BeaconRename.java & initialize it

--- src/dev/_2lstudios/BeaconRename.java
+++ src/dev/_2lstudios/BeaconRename.java
@@ -2,9 +2,11 @@ package dev._2lstudios;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import dev._2lstudios.listener.BeaconRenameListeners;
+
 public class BeaconRename extends JavaPlugin {
     @Override
     public void onEnable() {
-        
+       BeaconRenameListeners.initialize(this); 
     }
 }
---
+++ src/dev/_2lstudios/listener/BeaconRenameListeners.java
@@ -0,0 +1,10 @@
+package dev._2lstudios.listener;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+
+public class BeaconRenameListeners {
+    public static void initialize(final Plugin plugin) {
+        final PluginManager pluginManager = plugin.getServer().getPluginManager();
+    }
+}
