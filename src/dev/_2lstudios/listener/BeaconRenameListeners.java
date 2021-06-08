62b5a49133b76d664c9181f8efc0ffcbcfb27649
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-08 18:23:15 -0300

    add PlayerInteractListener

--- src/dev/_2lstudios/listener/BeaconRenameListeners.java
+++ src/dev/_2lstudios/listener/BeaconRenameListeners.java
@@ -3,8 +3,12 @@ package dev._2lstudios.listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
+import dev._2lstudios.rename.RenameManager;
+
 public class BeaconRenameListeners {
-    public static void initialize(final Plugin plugin) {
+    public static void initialize(final Plugin plugin, final RenameManager renameManager) {
         final PluginManager pluginManager = plugin.getServer().getPluginManager();
+
+        pluginManager.registerEvents(new PlayerInteractListener(renameManager), plugin);
     }
 }
---
+++ src/dev/_2lstudios/listener/PlayerInteractListener.java
@@ -0,0 +1,26 @@
+package dev._2lstudios.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+import dev._2lstudios.rename.RenameManager;
+
+public class PlayerInteractListener implements Listener {
+    private final RenameManager renameManager;
+
+    PlayerInteractListener(final RenameManager renameManager) {
+        this.renameManager = renameManager;
+    }
+
+    @EventHandler
+    public void onPlayerInteract(final PlayerInteractEvent event) {
+        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            final Player player = event.getPlayer();
+
+            renameManager.createTask(player);
+        }
+    }
+}
