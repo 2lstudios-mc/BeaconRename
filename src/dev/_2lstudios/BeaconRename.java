261bc88aaae00f0b293c474e69ae7183f9e1e1f5
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-08 18:00:46 -0300

    create BeaconRename.java

---
+++ src/dev/_2lstudios/BeaconRename.java
@@ -0,0 +1,10 @@
+package dev._2lstudios;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BeaconRename extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+}
