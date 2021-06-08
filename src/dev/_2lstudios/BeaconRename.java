95238cab3fa0394de99f3db5c09de6116e0c1f08
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-08 18:23:43 -0300

    initialize listeners with required RenameManager

--- src/dev/_2lstudios/BeaconRename.java
+++ src/dev/_2lstudios/BeaconRename.java
@@ -3,10 +3,13 @@ package dev._2lstudios;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import dev._2lstudios.listener.BeaconRenameListeners;
+import dev._2lstudios.rename.RenameManager;
 
 public class BeaconRename extends JavaPlugin {
     @Override
     public void onEnable() {
-       BeaconRenameListeners.initialize(this); 
+        final RenameManager renameManager = new RenameManager();
+
+        BeaconRenameListeners.initialize(this, renameManager);
     }
 }
