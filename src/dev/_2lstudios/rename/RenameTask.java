b8a1ed6fbcd132a3d9a2d4e094c9ee131449b755
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-09 16:52:24 -0300

    always check sword

--- src/dev/_2lstudios/rename/RenameTask.java
+++ src/dev/_2lstudios/rename/RenameTask.java
@@ -24,8 +24,19 @@ public class RenameTask {
         runs++;
 
         final String itemName = renameConfig.getItemName();
+        final PlayerInventory inventory = player.getInventory();
+        final int heldItemSlot = inventory.getHeldItemSlot();
+        final ItemStack item = inventory.getItem(heldItemSlot);
 
-        if (runs < retries) {
+        if (item == null || !BukkitUtils.isSword(item)) {
+            final Sound sound = renameConfig.getErrorSound();
+
+            player.sendMessage(renameConfig.getErrorMessage());
+
+            if (sound != null) {
+                player.playSound(player.getLocation(), sound, 1, 1);
+            }
+        } else if (runs < retries) {
             final Sound sound = renameConfig.getRetrySound();
 
             player.sendMessage(renameConfig.getRetryMessage(itemName));
@@ -34,30 +45,16 @@ public class RenameTask {
                 player.playSound(player.getLocation(), sound, 1, 1);
             }
         } else if (runs == retries) {
-            final PlayerInventory inventory = player.getInventory();
-            final int heldItemSlot = inventory.getHeldItemSlot();
-            final ItemStack item = inventory.getItem(heldItemSlot);
+            final Sound sound = renameConfig.getSuccessSound();
+            final ItemMeta itemMeta = item.getItemMeta();
 
-            if (item != null && BukkitUtils.isSword(item)) {
-                final Sound sound = renameConfig.getSuccessSound();
-                final ItemMeta itemMeta = item.getItemMeta();
+            itemMeta.setDisplayName(itemName);
+            item.setItemMeta(itemMeta);
+            inventory.setItem(heldItemSlot, item);
+            player.sendMessage(renameConfig.getSuccessMessage(itemName));
 
-                itemMeta.setDisplayName(itemName);
-                item.setItemMeta(itemMeta);
-                inventory.setItem(heldItemSlot, item);
-                player.sendMessage(renameConfig.getSuccessMessage(itemName));
-
-                if (sound != null) {
-                    player.playSound(player.getLocation(), sound, 1, 1);
-                }
-            } else {
-                final Sound sound = renameConfig.getErrorSound();
-
-                player.sendMessage(renameConfig.getErrorMessage());
-
-                if (sound != null) {
-                    player.playSound(player.getLocation(), sound, 1, 1);
-                }
+            if (sound != null) {
+                player.playSound(player.getLocation(), sound, 1, 1);
             }
         }
 
