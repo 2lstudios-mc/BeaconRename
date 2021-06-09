a0efc790731865745948b4059f2f81f86acf507d
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-08 21:11:28 -0300

    update RenameConfig.java

--- src/dev/_2lstudios/rename/RenameConfig.java
+++ src/dev/_2lstudios/rename/RenameConfig.java
@@ -2,16 +2,24 @@ package dev._2lstudios.rename;
 
 import java.util.List;
 
+import org.bukkit.Sound;
 import org.bukkit.configuration.Configuration;
 
 import dev._2lstudios.utils.ChatColorUtils;
+import dev._2lstudios.utils.ListUtils;
 
 public class RenameConfig {
     private final List<String> retryMessages;
     private final List<String> successMessages;
     private final List<String> errorMessages;
     private final List<String> itemNames;
+
+    private final List<Sound> retrySounds;
+    private final List<Sound> successSounds;
+    private final List<Sound> errorSounds;
+
     private final int retries;
+
     private final long delay;
 
     public RenameConfig(final Configuration config) {
@@ -19,7 +27,13 @@ public class RenameConfig {
         successMessages = ChatColorUtils.translateColorCodes(config.getStringList("success_messages"));
         errorMessages = ChatColorUtils.translateColorCodes(config.getStringList("error_messages"));
         itemNames = ChatColorUtils.translateColorCodes(config.getStringList("item_names"));
+
+        retrySounds = ListUtils.toSoundList(config.getStringList("retry_sounds"));
+        successSounds = ListUtils.toSoundList(config.getStringList("success_sounds"));
+        errorSounds = ListUtils.toSoundList(config.getStringList("error_sounds"));
+
         retries = config.getInt("retries", 4);
+
         delay = config.getLong("delay", 5L);
     }
 
@@ -31,29 +45,35 @@ public class RenameConfig {
         return retries;
     }
 
-    private String getRandom(final List<String> list) {
-        final int index = (int) (Math.random() * list.size());
-
-        return list.get(index);
-    }
-
     private String replacePlaceholders(final String string, final String itemName) {
         return ChatColorUtils.translateColorCodes(string.replace("%item_name%", itemName));
     }
 
     public String getRetryMessage(final String itemName) {
-        return replacePlaceholders(getRandom(retryMessages), itemName);
+        return replacePlaceholders(ListUtils.getRandomString(retryMessages), itemName);
     }
 
     public String getSuccessMessage(final String itemName) {
-        return replacePlaceholders(getRandom(successMessages), itemName);
+        return replacePlaceholders(ListUtils.getRandomString(successMessages), itemName);
     }
 
     public String getErrorMessage() {
-        return getRandom(errorMessages);
+        return ChatColorUtils.translateColorCodes(ListUtils.getRandomString(errorMessages));
     }
 
     public String getItemName() {
-        return getRandom(itemNames);
+        return ChatColorUtils.translateColorCodes(ListUtils.getRandomString(itemNames));
+    }
+
+    public Sound getRetrySound() {
+        return ListUtils.getRandomSound(retrySounds);
+    }
+
+    public Sound getSuccessSound() {
+        return ListUtils.getRandomSound(successSounds);
+    }
+
+    public Sound getErrorSound() {
+        return ListUtils.getRandomSound(errorSounds);
     }
 }
