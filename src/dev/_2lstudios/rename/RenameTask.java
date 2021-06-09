77d59e6f085d0ffb7317a9ac5b061e820db1a7b3
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-08 21:11:52 -0300

    update RenameTask.java

--- src/dev/_2lstudios/rename/RenameTask.java
+++ src/dev/_2lstudios/rename/RenameTask.java
@@ -1,8 +1,10 @@
 package dev._2lstudios.rename;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class RenameTask {
     private final RenameConfig renameConfig;
@@ -24,17 +26,38 @@ public class RenameTask {
         final String itemName = renameConfig.getItemName();
 
         if (runs < retries) {
+            final Sound sound = renameConfig.getRetrySound();
+
             player.sendMessage(renameConfig.getRetryMessage(itemName));
+
+            if (sound != null) {
+                player.playSound(player.getLocation(), sound, 1, 1);
+            }
         } else if (runs == retries) {
             final PlayerInventory inventory = player.getInventory();
             final int heldItemSlot = inventory.getHeldItemSlot();
             final ItemStack itemInHand = inventory.getItem(heldItemSlot);
 
             if (itemInHand != null && itemInHand.isSimilar(item)) {
+                final Sound sound = renameConfig.getSuccessSound();
+                final ItemMeta itemMeta = item.getItemMeta();
+
+                itemMeta.setDisplayName(itemName);
+                item.setItemMeta(itemMeta);
                 inventory.setItem(heldItemSlot, item);
                 player.sendMessage(renameConfig.getSuccessMessage(itemName));
+
+                if (sound != null) {
+                    player.playSound(player.getLocation(), sound, 1, 1);
+                }
             } else {
+                final Sound sound = renameConfig.getErrorSound();
+
                 player.sendMessage(renameConfig.getErrorMessage());
+
+                if (sound != null) {
+                    player.playSound(player.getLocation(), sound, 1, 1);
+                }
             }
         }
 
